2493019422cb0a148f1bc3cb325ab2eed0ede369
Jayasri P <jayaporalla921@gmail.com>
2025-08-02 10:43:13 +0530

    Module7 code updated

---
+++ module7/squarePattern.java
@@ -0,0 +1,17 @@
+package module7;
+import java.util.Scanner;
+
+public class squarePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
---
+++ module7/tableNumber.java
@@ -0,0 +1,14 @@
+package module7;
+import java.util.Scanner;
+
+public class tableNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= 10; i++){
+            System.out.print(n * i + " ");
+        }
+        sc.close();
+    }
+}

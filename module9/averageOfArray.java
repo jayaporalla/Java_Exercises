c549d43f0ec717cfcc1724c8992a53dd39a10ade
Jayasri P <jayaporalla921@gmail.com>
2025-08-08 16:37:45 +0530

    Arrays topic module9 code updated

---
+++ module9/averageOfArray.java
@@ -0,0 +1,23 @@
+package module9;
+import java.util.Scanner;
+
+public class averageOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        System.out.print("Enter array elements: " + "");
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        int average = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+        }
+        average = sum / n;
+        System.out.println("Average of Array: " + average);
+        sc.close();
+    }
+}
---
+++ module9/maximumNumberInArray.java
@@ -0,0 +1,23 @@
+package module9;
+import java.util.Scanner;
+
+public class maximumNumberInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        System.out.print("Enter array elements: " + "");
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for(int i = 1; i < n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum number: " + max);
+        sc.close();
+    }
+}

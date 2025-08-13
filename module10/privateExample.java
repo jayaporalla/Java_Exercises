892c418210d42df960836c591b711c4f39f14a8a
Jayasri P <jayaporalla921@gmail.com>
2025-08-13 15:30:45 +0530

    class & objects module10 code updated

---
+++ module10/privateExample.java
@@ -0,0 +1,14 @@
+package module10;
+
+class Point {
+    // private int x;
+    int x;
+}
+
+public class privateExample {
+    public static void main(String[] args) {
+        Point p = new Point();
+        // System.out.println(p.x);
+        System.out.println(p.x);
+    }
+}
---
+++ module10/publicExample.java
@@ -0,0 +1,8 @@
+package module10;
+
+public class publicExample {
+    public static void main(String[] args) {
+        Test t = new Test(10, 20);
+        t.print();  // here i have used constructoExample method as it is public so can use anywhere
+    }
+}

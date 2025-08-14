873085ae3d4e286f8152a5a8251c22e7b6dbb6e4
Jayasri P <jayaporalla921@gmail.com>
2025-08-14 18:47:18 +0530

    Advanced Topics module code half updated

---
+++ AdvancedTopics/checkPrimeNextPrime.java
@@ -0,0 +1,26 @@
+package AdvancedTopics;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class checkPrimeNextPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int N = sc.nextInt();
+        System.out.print("Checking entered number whether prime or not, true if it's prime and false if it's not a prime: ");
+        System.out.print(isPrime(N));
+        System.out.print("\nThe prime number after " + N + " is " + nextPrime(N));
+        sc.close();
+    }
+
+    private static int nextPrime(int N){
+        BigInteger n = BigInteger.valueOf(N);
+        String s = n.nextProbablePrime().toString();
+        return Integer.parseInt(s);
+    }
+
+    private static boolean isPrime(int N){
+        BigInteger n = BigInteger.valueOf(N);
+        return n.isProbablePrime(1);
+    }
+}
---
+++ AdvancedTopics/fibonacciBigInteger.java
@@ -0,0 +1,29 @@
+package AdvancedTopics;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class fibonacciBigInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n));
+        sc.close();
+    }
+
+    private static BigInteger fibonacci(int n){
+
+        if(n == 0) return BigInteger.ZERO;
+        if(n == 1) return BigInteger.ONE;
+
+        BigInteger A = BigInteger.ZERO;
+        BigInteger B = BigInteger.ONE;
+        BigInteger result = BigInteger.ZERO;
+        for(int i = 2; i <= n; i++){
+            result = A.add(B);
+            A = B;
+            B = result;
+        }
+        return result;
+    }
+}
